7f5b532896dafc0e22ab5c6d71e7aa2f53eade14
hernanxd7 <hernanxs98@gmail.com>
2025-06-23 12:40:01 -0600

    Configuración CI/CD y GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 cambia esto según tu repositorio
+const repoName = 'landing-page-cicd'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, // 👈 esta línea es clave
 })
